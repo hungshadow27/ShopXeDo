e32099c7b87a124b0edbf500bd4da2aab581346c
Pham Viet Hung <hungphamviet11@gmail.com>
2025-03-17 20:32:32 +0700

    update sql file

--- shopxedo.sql
+++ shopxedo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2025 at 03:46 PM
+-- Generation Time: Mar 17, 2025 at 02:31 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `shopxedo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`) VALUES
+(1, 1, 1, 2),
+(3, 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -47,6 +68,54 @@ INSERT INTO `category` (`id`, `name`, `description`, `image`, `featured`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `shipping_address` varchar(2550) NOT NULL,
+  `total_cost` int(15) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
+  `ispaid` varchar(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `finish_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `product`, `shipping_address`, `total_cost`, `status`, `ispaid`, `created_at`, `finish_date`) VALUES
+(7, 1, 'a:1:{i:0;a:2:{s:2:\"id\";s:1:\"1\";s:8:\"quantity\";s:1:\"1\";}}', 'ngũ phúc-kiến thụy-hải phòng', 1530000, 2, '0', '2025-03-17 16:38:35', '2025-03-17 16:39:00'),
+(8, 1, 'a:1:{i:0;a:2:{s:2:\"id\";s:1:\"1\";s:8:\"quantity\";s:1:\"2\";}}', 'ngũ phúc-kiến thụy-hải phòng', 3030000, 0, '0', '2025-03-17 20:19:59', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `thumbnail`, `created_at`, `updated_at`) VALUES
+(1, 'TÉT', '<p><a href=\"https://xedodep.com/wp-content/uploads/2021/10/Untitled-1.jpg\"><img alt=\"\" src=\"https://xedodep.com/wp-content/uploads/2021/10/Untitled-1.jpg\" /></a></p>\r\n\r\n<p><span style=\"font-size:48px\"><span style=\"color:#1abc9c\"><strong>Test 1</strong></span></span></p>\r\n\r\n<p>jhahiufhwbfahfuoqhoungauhfo</p>\r\n\r\n<p><span style=\"color:#9b59b6\"><span style=\"font-size:72px\"><strong>Test 2</strong></span></span></p>\r\n\r\n<p>faohowhjoijqwnoahoid</p>', 'https://xedodep.com/wp-content/uploads/2021/10/Untitled-1.jpg', '2025-03-17 03:05:03', '2025-03-17 09:46:23');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -66,7 +135,34 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `description`, `image`, `price`, `sale`, `stock_quantity`, `category_id`) VALUES
-(1, 'Combo lên đĩa (LV1) NEW 2022', 'Thông số cơ bản  Lắp cho xe: Nhiều dòng xe Model: Combo lv1 Thương hiệu: Honda, Yamaha Loại:               1 Mô tả chi tiết  Combo đùm lên đĩa lv1 2022 được XĐĐ lựa chọn tối ưu nhất tiết kiệm nhất và đẹp hơn mẫu 2019 Vài điều về sản […]', 'a:3:{i:0;s:27:\"combolendialv1new2022-1.jpg\";i:1;s:27:\"combolendialv1new2022-2.jpg\";i:2;s:27:\"combolendialv1new2022-3.jpg\";}', 1500000.00, 0.00, 11, 1);
+(1, 'Combo lên đĩa (LV1) NEW 2022', '<h1><span style=\"font-size:36px\"><span style=\"color:#e74c3c\"><strong>M&ocirc; tả chi tiết&nbsp;</strong></span></span></h1>\r\n\r\n<p>Combo đ&ugrave;m l&ecirc;n đĩa lv1 2022 được XĐĐ lựa chọn tối ưu nhất tiết kiệm nhất v&agrave; đẹp hơn mẫu 2019</p>\r\n\r\n<h1><strong>V&agrave;i điều về sản phẩm</strong></h1>\r\n\r\n<p>Combo lv1 mới kh&aacute;c với combo lv1 củ ở đĩa thắng trước đẹp hơn khi sử dụng đĩa phay lỗ th&aacute;i, v&agrave; mẫu phuộc trước được lựa chọn tốt hơn bởi thương hiệu DPR bảo h&agrave;nh 6 th&aacute;ng</p>\r\n\r\n<h1><strong>C&aacute;c bạn từ Channel Xe độ đẹp đ&aacute;nh gi&aacute;</strong></h1>\r\n\r\n<p>Combo lv1 NEW c&oacute; gi&aacute; th&agrave;nh thấp nhất d&agrave;nh cho học sinh sinh vi&ecirc;n muốn từ phanh đ&ugrave;m l&ecirc;n phanh đĩa trước v&agrave; nh&igrave;n đẹp mắt với đĩa kiểu trước</p>\r\n\r\n<h1><strong>Combo lv1 new gồm c&oacute;:</strong></h1>\r\n\r\n<ol>\r\n	<li>Cặp phuộc trước DPR ( Bảo h&agrave;nh 6 th&aacute;ng )</li>\r\n	<li>Tay thắng dầu</li>\r\n	<li>D&acirc;y dầu</li>\r\n	<li>Heo 1 pis</li>\r\n	<li>Đ&ugrave;m trước</li>\r\n	<li>Căm osaki ( 1 b&aacute;nh trước )</li>\r\n	<li>Đĩa kiểu th&aacute;i</li>\r\n</ol>', 'a:3:{i:0;s:27:\"combolendialv1new2022-1.jpg\";i:1;s:27:\"combolendialv1new2022-2.jpg\";i:2;s:27:\"combolendialv1new2022-3.jpg\";}', 1500000.00, 0.00, 11, 1),
+(2, 'Bao tay Baracuda *Chính hãng', '', 'a:3:{i:0;s:19:\"baotaybaracuda1.jpg\";i:1;s:19:\"baotaybaracuda2.jpg\";i:2;s:19:\"baotaybaracuda3.jpg\";}', 500000.00, 0.00, 11, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `star` int(1) DEFAULT NULL,
+  `comment` varchar(2550) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`id`, `order_id`, `user_id`, `product_id`, `star`, `comment`, `created_at`) VALUES
+(1, 3, 1, 1, 4, 'fff', '2025-03-16 22:13:14'),
+(2, 3, 1, 2, 5, 'aa', '2025-03-16 22:20:09'),
+(3, 4, 1, 1, 5, 'ggg', '2025-03-16 23:10:51'),
+(4, 7, 1, 1, 5, 'ggg', '2025-03-17 16:39:11');
 
 -- --------------------------------------------------------
 
@@ -92,11 +188,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `role`, `name`, `date_of_birth`, `gender`, `address`, `phone_number`, `created_at`) VALUES
-(1, 'admin', '123', 'admin', 'ADMIN', '0000-00-00', '0', '', '', '2025-03-10');
+(1, 'admin', '123', 'admin', 'Admin', '2025-03-18', '1', 'ngũ phúc-kiến thụy-hải phòng', '0987654321', '2025-03-17'),
+(5, 'hungngu', '123', 'customer', 'Phạm Hùng', '2025-03-12', '1', 'ngũ phúc-kiến thụy-hải phòng', '4213213', '2025-03-17');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `category`
@@ -105,9 +208,27 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -121,22 +242,46 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
